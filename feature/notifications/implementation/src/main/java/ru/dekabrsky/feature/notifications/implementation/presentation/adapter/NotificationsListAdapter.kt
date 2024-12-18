a06629cf0382f1ba9e853264dec2cf9567d06f85
Denis Matafonov <dmatafonov2000@mail.ru>
2024-12-19 00:19:51 +0500

    try to fix dependencies

--- feature/notifications/implementation/src/main/java/ru/dekabrsky/feature/notifications/implementation/presentation/adapter/NotificationsListAdapter.kt
+++ feature/notifications/implementation/src/main/java/ru/dekabrsky/feature/notifications/implementation/presentation/adapter/NotificationsListAdapter.kt
@@ -4,7 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.chauthai.swipereveallayout.ViewBinderHelper
+import io.github.rexmtorres.android.swipereveallayout.ViewBinderHelper
 import main.utils.orZero
 import main.utils.setBoolVisibility
 import main.utils.setIsCheckedWithoutEffects
